9c1d73702e4588246224b745c22d56d830108879
Roman1Roman <127185503+Roman1Roman@users.noreply.github.com>
2023-11-21 00:12:06 +0300

    added some parameters

--- homework/test_fixtures.py
+++ homework/test_fixtures.py
@@ -1,11 +1,32 @@
+import pytest
+from selene.support.shared import browser
+
 """
 Сделайте разные фикстуры для каждого теста, которые выставят размеры окна браузера
 """
 
 
-def test_github_desktop():
-    pass
+@pytest.fixture()
+def browser_desktop():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.open('https://github.com')
+
+    yield
+    browser.quit()
+
+
+@pytest.fixture()
+def browser_mobile():
+    browser.config.window_width = 1000
+    browser.config.window_height = 2532
+    browser.open('https://github.com')
+
+
+def test_github_desktop(browser_mobile):
+    browser.element("//button[@aria-label='Toggle navigation' and @data-view-component='true']").click()
+    browser.element("//a[@href='/login']").click()
 
 
 def test_github_mobile():
-    pass
+    browser.element("//a[@href='/login']").click()
--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -1,17 +1,28 @@
 """
 Переопределите параметр с помощью indirect параметризации на уровне теста
 """
+from selene.support.shared import browser
 import pytest
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.fixture(params=['desktop_1', 'mobile_1'])
+def browser_scale(request):
+    if request.param == 'desktop_1':
+        browser.config.window_width = 1920
+        browser.config.window_height = 1080
+        browser.open('https://github.com')
+    if request.param == 'mobile_1':
+        browser.config.window_width = 1000
+        browser.config.window_height = 2532
+        browser.open('https://github.com')
 
 
-def test_github_desktop(browser):
-    pass
+@pytest.mark.parametrize('browser_scale', ["desktop_1"], indirect=True)
+def test_open_github_parametrized_desktop(browser_scale):
+    browser.element("//a[@href='/login']").click()
 
 
-def test_github_mobile(browser):
-    pass
+@pytest.mark.parametrize('browser_scale', ["mobile_1"], indirect=True)
+def test_test_open_github_parametrized_mobile(browser_scale):
+    browser.element("//button[@aria-label='Toggle navigation' and @data-view-component='true']").click()
+    browser.element("//a[@href='/login']").click()
