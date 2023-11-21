f66c21304381b15e9ecf63b48c9dbbd807f01227
Roman0Roman <127503924+Roman0Roman@users.noreply.github.com>
2023-11-21 16:03:54 +0300

    starting skipping test module

--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -5,15 +5,23 @@ from selene.support.shared import browser
 import pytest
 
 
-@pytest.fixture(params=['desktop_1', 'mobile_1'])
+@pytest.fixture(params=['desktop_1', 'mobile_1', 'desktop_2', 'mobile_2'])
 def browser_scale(request):
     if request.param == 'desktop_1':
         browser.config.window_width = 1920
         browser.config.window_height = 1080
         browser.open('https://github.com')
-    if request.param == 'mobile_1':
+    elif request.param == 'desktop_2':
+        browser.config.window_width = 1280
+        browser.config.window_height = 720
+        browser.open('https://github.com')
+    elif request.param == 'mobile_1':
         browser.config.window_width = 1000
         browser.config.window_height = 2532
+        browser.open('https://github.com')
+    elif request.param == 'mobile_2':
+        browser.config.window_width = 1000
+        browser.config.window_height = 2200
         browser.open('https://github.com')
 
 
@@ -22,7 +30,7 @@ def test_open_github_parametrized_desktop(browser_scale):
     browser.element("//a[@href='/login']").click()
 
 
-@pytest.mark.parametrize('browser_scale', ["mobile_1"], indirect=True)
-def test_test_open_github_parametrized_mobile(browser_scale):
+@pytest.mark.parametrize('browser_scale', ["mobile_2"], indirect=True)
+def test_open_github_parametrized_mobile(browser_scale):
     browser.element("//button[@aria-label='Toggle navigation' and @data-view-component='true']").click()
     browser.element("//a[@href='/login']").click()
--- homework/test_skip.py
+++ homework/test_skip.py
@@ -3,16 +3,46 @@
 Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
 """
 import pytest
+from selene.support.shared import browser
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.fixture(params=['desktop_1', 'desktop_2', 'mobile_1', 'mobile_2'], scope='session')
+def browser_config(request):
+    if request.param == 'desktop_1':
+        browser.config.window_height = 1080
+        browser.config.window_width = 1920
+        # return request.param
+    elif request.param == 'desktop_2':
+        browser.config.window_height = 720
+        browser.config.window_width = 1280
+        # browser.open('https://github.com')
+        # return request.param
+    elif request.param == 'mobile_1':
+        browser.config.window_height = 2532
+        browser.config.window_width = 1000
+        # browser.open('https://github.com')
+        # return request.param
+    elif request.param == 'mobile_2':
+        browser.config.window_height = 2200
+        browser.config.window_width = 1000
+        # browser.open('https://github.com')
+
+    browser.open('https://github.com')
+    return request.param
+    # yield
+    # browser.quit()
 
 
-def test_github_desktop(browser):
-    pass
+# @pytest.mark.parametrize('browser_config')
+def test_github_desktop(browser_config):
+    if browser_config == 'mobile_1' or browser_config == 'mobile_2':
+        pytest.skip(reason='mobile scale not for desktop')
+    browser.element("//a[@href='/login']").click()
 
 
-def test_github_mobile(browser):
-    pass
+# @pytest.mark.parametrize('browser_config')
+def test_github_mobile(browser_config):
+    if browser_config.param == 'desktop_1' or browser_config.param == 'desktop_2':
+        pytest.skip(reason='desktop scale not for mobile')
+    browser.element("//button[@aria-label='Toggle navigation' and @data-view-component='true']").click()
+    browser.element("//a[@href='/login']").click()
